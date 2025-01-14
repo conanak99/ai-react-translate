557673aad7016173cba9450d30a6b5b38f7396a9
Harry Pham <harry.pham@transferwise.com>
2025-01-14 12:03:31 +0800

    Add test and fix url logic

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,29 +7,20 @@ export function cn(...inputs: ClassValue[]) {
 
 
 export function getNextChapterUrl(inputURL: string) {
-	// Example input `https://truyenyy.vip/truyen/thinh-cong-tu-tram-yeu/chuong-309.html`
-  // https://www.bq01.cc/index/38697/73.html
-	// Get chapter number from url using regex
-
 	const matches = [...inputURL.matchAll(/\d+/g)];
 	const chapterNumber = matches[matches.length - 1]?.[0];
 
 	// Change chapter number by change value
 	const newChapterNumber = Number(chapterNumber) + 1;
 
-	// Replace chapter number in url
 	const newUrl = inputURL.replace(
-		String(chapterNumber),
+		new RegExp(`${chapterNumber}(?=[^\\d]*$)`),
 		newChapterNumber.toString(),
 	);
 	return newUrl;
 }
 
 export function getPreviousChapterUrl(inputURL: string) {
-	// Example input `https://truyenyy.vip/truyen/thinh-cong-tu-tram-yeu/chuong-309.html`
-  // https://www.bq01.cc/index/38697/73.html
-	// Get chapter number from url using regex
-
 	const matches = [...inputURL.matchAll(/\d+/g)];
 	const chapterNumber = matches[matches.length - 1]?.[0];
 
@@ -38,7 +29,7 @@ export function getPreviousChapterUrl(inputURL: string) {
 
 	// Replace chapter number in url
 	const newUrl = inputURL.replace(
-		String(chapterNumber),
+		new RegExp(`${chapterNumber}(?=[^\\d]*$)`),
 		newChapterNumber.toString(),
 	);
 	return newUrl;
